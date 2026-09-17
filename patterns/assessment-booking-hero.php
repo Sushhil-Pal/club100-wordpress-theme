91e43b678c13063d25f99edfe3994c9def7ad1e3
Sushhil-Pal <sushil.pal@gmail.com>
2026-09-17 10:42:55 +0530

    Removing Test

--- patterns/assessment-booking-hero.php
+++ patterns/assessment-booking-hero.php
@@ -70,7 +70,7 @@
 
 				<p>
 					Complete the form and our team will contact you to confirm
-					your assessment Test1.
+					your assessment.
 				</p>
 
 				<div class="club100-assessment-form">
