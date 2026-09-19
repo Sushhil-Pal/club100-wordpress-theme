ac349e21c7afdc2153bc2db740237436c09f946d
Sushhil-Pal <sushil.pal@gmail.com>
2026-09-19 13:45:11 +0530

    Sample report

--- patterns/fitness-report.php
+++ patterns/fitness-report.php
@@ -124,7 +124,10 @@
 
 					<!-- wp:button -->
 					<div class="wp-block-button">
-						<a class="wp-block-button__link wp-element-button" href="/wp-content/uploads/2026/09/club100-sample-fitness-report.pdf">
+						<a class="wp-block-button__link wp-element-button" 
+						target="_blank"
+  						rel="noopener"
+						href="/wp-content/uploads/2026/09/club100-sample-fitness-report.pdf">
 							View Sample Fitness Report
 						</a>
 					</div>
